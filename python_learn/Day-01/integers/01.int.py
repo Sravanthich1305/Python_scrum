f78e330d3081af5623fb549fe5e97eccca504d34
Sravanthi chakravaram <sravanthich1305@gmail.com>
2024-01-09 18:22:46 -0500

    comment

---
+++ python_learn/Day-01/integers/01.int.py
@@ -0,0 +1,26 @@
+##Arthimatical solutions using integers
+num1 = 10
+num2 = 2
+#addition
+res1 = num1 + num2
+print(res1)
+##sub
+res2 = num2 - num1
+res3 = num1 - num2
+print(res2)
+print(res3)
+##divison
+res4 = num1 / num2
+print(res4)
+##multiplication
+res5 = num1 * num2
+print(res5)
+#percentage
+res6 = num1 % num2
+print(res6)
+##Abolute value 
+res7 = abs(-7)
+print(res7)
+#Modulus of Remainder
+res8 = num1 % num2 
+print("Modulus (Remainder):", res8)
